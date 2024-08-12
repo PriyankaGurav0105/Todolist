189cf36dcdca08d3fcf38fd436adddcdc5c827c6
PriyankaGurav0105 <priyankagurav0105@gmail.com>
2024-08-12 15:53:20 +0530

    Add task function added

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 const taskInput = document.getElementById("task");
 const addBtn = document.getElementById("add");
 const taskList = document.getElementById("taskList");
+
+//Add task
+addBtn.addEventListener("click", function(){
+    const taskText = taskInput.value.trim();
+    if(taslText !==""){
+        createTask(taskText);
+        taskInput.value ="";
+    }
+});
